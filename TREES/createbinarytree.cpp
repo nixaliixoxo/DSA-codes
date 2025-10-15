585f42b864573d2fefca1269bc9a04bbc82c91b8
NEELAKSHI SACHDEVA <neelakshisachdeva@gmail.com>
2025-10-15 23:33:47 +0530

    trees starter

---
+++ TREES/createbinarytree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        left=right=NULL;
+    }
+};
+
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    int firstChild, secondChild;
+    queue <Node*> q;
+    Node* root = new Node(x);
+    q.push(root);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        cin >> firstChild;
+        if(firstChild != -1){
+            temp->left = new Node(firstChild);
+            q.push(temp->left);
+        }
+        cin >> secondChild;
+        if(secondChild != -1){
+            temp->right = new Node(secondChild);
+            q.push(temp->right);
+        }
+    }
+}
